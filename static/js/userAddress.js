d593b59427d9ff9968c7da059037d87e3dc6311d
HangWei <hangwei@HangWeideMacBook-Pro.local>
2015-11-19 14:21:39 +0800

    修改user.css

--- static/js/userAddress.js
+++ static/js/userAddress.js
@@ -4,19 +4,12 @@
 (function($){
     var app = angular.module('myApp', []);
     app.controller('myCtrl', ['$scope', function($scope) {
+
         $('.false-show').show();
-        $scope.back={}
-        $scope.reset=function(){
-            $scope.back.detail=$scope.send.detail;
-            $scope.back.code=$scope.send.code;
-            $scope.back.name=$scope.send.name;
-            $scope.back.phone=$scope.send.phone;
-            $scope.back.province=$scope.send.province;
-            $scope.back.city=$scope.send.city;
-            $scope.back.area=$scope.send.area;
-        }
+
         $scope.save = function() {
             // check to make sure the form is completely valid
+
             if ($scope.addressForm.$valid) {
                 alert('success');
             }else{
@@ -25,6 +18,7 @@
 
         };
         console.log($scope)
+
     }]);
 })(jQuery)
 
