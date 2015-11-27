afc80d7545ffb77f2ed9d6e85bb3e49fb851437a
ZiQing089 <199220721@qq.com>
2015-11-27 10:15:53 +0800

    添加去往邮箱页面

--- static/js/expressCheck.js
+++ static/js/expressCheck.js
@@ -1,3 +1,9 @@
 $('.check-con-tabNav li').on('click',function () {
-	var tabIndex = $(this).index();
+	var _self = $(this);
+	var tabIndex = _self.index();
+	//console.log(tabIndex);
+	_self.addClass('check-con-tabActive');
+	_self.siblings().removeClass('check-con-tabActive');
+	$(".check-tabCon-pannel").hide();
+	$(".check-tabCon-pannel").eq(tabIndex).show();
 })
